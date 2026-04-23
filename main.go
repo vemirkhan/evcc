f5f3b1a4d71c591d5a5898e8cd7cb6d169312164
Vemirk Han <vemirkhan@users.noreply.github.com>
2026-04-23 23:56:54 +0800

    feat: add main entry point for evcc application

--- main.go
+++ main.go
@@ -1,45 +1,17 @@
 package main
 
 import (
-	"embed"
-	"io"
-	"io/fs"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/evcc-io/evcc/cmd"
-	"github.com/evcc-io/evcc/server/assets"
-	_ "golang.org/x/crypto/x509roots/fallback" // fallback certificates
 )
 
-var (
-	//go:embed dist
-	web embed.FS
-
-	//go:embed i18n/*.json
-	i18n embed.FS
-)
-
-// init loads embedded assets unless live assets are already loaded
-func init() {
-	if !assets.Live() {
-		var err error
-
-		assets.Web, err = fs.Sub(web, "dist")
-		if err != nil {
-			panic(err)
-		}
-
-		assets.I18n, err = fs.Sub(i18n, "i18n")
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
+// main is the entry point for evcc - EV Charge Controller
+// It delegates to the cmd package which uses cobra for CLI handling
 func main() {
-	// suppress deprecated: golang.org/x/oauth2: Transport.CancelRequest no longer does anything; use contexts
-	// see https://github.com/golang/oauth2/issues/487
-	log.SetOutput(io.Discard)
-
-	cmd.Execute()
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
